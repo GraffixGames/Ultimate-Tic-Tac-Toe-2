889d8e46b090f9bc91e4dffd3862ce8c259115a7
Noah Graff <graffixgames@gmai.com>
2017-12-08 10:13:44 -0500

    Initial Commit

---
+++ Ultimate-Tic-Tac-Toe-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ultimate-Tic-Tac-Toe-2
+//
+//  Created by  on 12/8/17.
+//  Copyright © 2017 Hi-Crew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
